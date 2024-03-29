9dbbc58efd7d3a430e1be0b9965b88573be68543
scott <scott.gordon72@outlook.com>
2024-03-29 14:11:46 -0400

    update image analysis

--- Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+++ Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
@@ -7,6 +7,9 @@ from azure.core.exceptions import HttpResponseError
 import requests
 
 # Import namespaces
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 
 def main():
@@ -27,11 +30,14 @@ def main():
             image_data = f.read()
 
         # Authenticate Azure AI Vision client
+            cv_client = ImageAnalysisClient(
+                endpoint=ai_endpoint,
+                credential=AzureKeyCredential(ai_key)
+            )
 
-        
         # Analyze image
         AnalyzeImage(image_file, image_data, cv_client)
-        
+
         # Background removal
         BackgroundForeground(ai_endpoint, ai_key, image_file)
 
@@ -44,7 +50,15 @@ def AnalyzeImage(image_filename, image_data, cv_client):
 
     try:
         # Get result with specified features to be retrieved
-        
+        result = cv_client.analyze(
+            image_data=image_data,
+            visual_features=[
+                VisualFeatures.CAPTION,
+                VisualFeatures.DENSE_CAPTIONS,
+                VisualFeatures.TAGS,
+                VisualFeatures.OBJECTS,
+                VisualFeatures.PEOPLE]
+        )
 
     except HttpResponseError as e:
         print(f"Status code: {e.status_code}")
@@ -52,15 +66,32 @@ def AnalyzeImage(image_filename, image_data, cv_client):
         print(f"Message: {e.error.message}")
 
     # Display analysis results
-    
+    # Get image captions
+    if result.caption is not None:
+        print("\nCaption:")
+        print(" Caption: '{}' (confidence: {:.2f}%)".format(
+            result.caption.text, result.caption.confidence * 100))
+
+    # Get image dense captions
+    if result.dense_captions is not None:
+        print("\nDense Captions:")
+        for caption in result.dense_captions.list:
+            print(" Caption: '{}' (confidence: {:.2f}%)".format(
+                caption.text, caption.confidence * 100))
+
+    # Get image tags
+
+    # Get image objects
+
+    # Get image people
+
 
 def BackgroundForeground(endpoint, key, image_file):
     # Define the API version and mode
     api_version = "2023-02-01-preview"
-    mode="backgroundRemoval" # Can be "foregroundMatting" or "backgroundRemoval"
-    
+    mode = "backgroundRemoval"  # Can be "foregroundMatting" or "backgroundRemoval"
+
     # Remove the background from the image or generate a foreground matte
-    
 
 
 if __name__ == "__main__":
